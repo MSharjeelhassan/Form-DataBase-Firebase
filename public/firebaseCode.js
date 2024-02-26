326c328468ffa42e0850951951b4fd304a13b194
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2024-02-26 20:06:12 +0500

    Working started

---
+++ public/firebaseCode.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyADA_eXml_S6wYedac2koc3f4zyG31_V74",
+    authDomain: "form-realtime-database.firebaseapp.com",
+    projectId: "form-realtime-database",
+    storageBucket: "form-realtime-database.appspot.com",
+    messagingSenderId: "504329894503",
+    appId: "1:504329894503:web:763d2bca2cf512966a72c2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
