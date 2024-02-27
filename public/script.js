bae18c907d1136d4a328c619c142f75405d6bb36
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2024-02-27 20:09:18 +0500

    working

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const selectCity = document.querySelector('.details select');
+// console.log(selectCity);
+// console.log(selectCity.value);
+const fullname = document.querySelector('.details input');
+// console.log(fullname)
+// const email = document.querySelector('.details input[type="text"]:nth-of type(2)');
+const email = document.querySelector('.details input[type="email"]');
+// console.log(email)
+// const 
